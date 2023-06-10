1bf1baa2b33d26abe155a0980b3dd4d99684f2d5
Felipe Barbosa do Nascimento <dfelipebdn@gmail.com>
2023-06-10 00:01:27 -0300

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,27 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      'yellow-dark': '#C47F17',
+      yellow: '#DBAC2C',
+      'yellow-light': '#F1E9C9',
+
+      'purple-dark': '#4B2995',
+      purple: '#8047F8',
+      'purple-light': '#EBE5F9',
+
+      'base-title': '#272221',
+      'base-subtitle': '#403937',
+      'base-text': '#574F4D',
+      'base-label': '#8D8686',
+
+      'base-hover': '#D7D5D5',
+      'base-button': '#E6E5E5',
+      'base-input': '#EDEDED',
+      'base-card': '#F3F2F2',
+
+      background: '#FAFAFA',
+      white: '#FFFFFF',
     },
   },
   plugins: [],
