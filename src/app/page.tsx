60bb0be8b4d228c5aa9cbcd021844dfbb1383450
Felipe Barbosa do Nascimento <dfelipebdn@gmail.com>
2023-06-18 15:08:43 -0300

    teste de implementação do urql com o next 13, sem sucesso

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
 'use client'
 import { CoffeeCart } from '@/components/coffeeCard'
 import { Hero } from '@/components/hero'
+import { gql, useQuery } from 'urql'
 
 export default function Home() {
+  const TESTE = useQuery({
+    query: gql`
+      query Teste {
+        coffeesPlural {
+          createdAt
+          name
+          id
+          price
+          typeCoffeesPlural(orderBy: typeName_ASC) {
+            createdAt
+            id
+            typeName
+          }
+        }
+      }
+    `,
+  })
+
+  console.log(TESTE)
+
   return (
     <>
       <Hero />
--- src/lib/urql.ts
+++ src/lib/urql.ts
@@ -7,6 +7,11 @@ const ssrCache = ssrExchange({ isClient: !isServerSide })
 const client = createClient({
   url: 'https://api-sa-east-1.hygraph.com/v2/cliyz37df0f0r01t61ss39qrr/master',
   exchanges: [cacheExchange, ssrCache, fetchExchange],
+  fetchOptions: {
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_HYGRAPH_TOKEN}`,
+    },
+  },
 })
 
 export { client, ssrCache }
